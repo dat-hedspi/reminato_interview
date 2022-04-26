f20f7b730915987aa107a237a13d827087b2d810
ta.tat.dat <dat.hedspi@gmail.com>
2022-04-26 12:39:27 +0700

    [feature] make layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
