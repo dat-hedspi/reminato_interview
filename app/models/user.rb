970200519ccaeeff6ab284bef8162dfbb57d4743
ta.tat.dat <dat.hedspi@gmail.com>
2022-04-26 12:58:16 +0700

    [bug] chanage authentication key from email to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable,
+    :validatable
   has_many :posts, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
@@ -10,4 +11,14 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship",
     foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
+
+  validates :name,  presence: true, uniqueness: true
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
