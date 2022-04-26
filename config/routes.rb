a311a474a3f82fcceab1a598697d9b7a595fde96
ta.tat.dat <dat.hedspi@gmail.com>
2022-04-26 10:49:40 +0700

    [feature]add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: "static_pages#index"
+  devise_for :users, path: '',
+    path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register', sign_up: 'sign_up' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
